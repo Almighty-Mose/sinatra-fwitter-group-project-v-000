75e8e9705c671353192d5ff4a1dd44e5cb37d0bd
Branden Mosley <branden.mosley1337@gmail.com>
2018-06-24 00:22:48 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
   get '/signup' do
     erb :'users/create_user'
+
+    redirect "/tweets"
   end
 
   get '/login' do
     erb :'users/login'
+  end
+
+  get '/tweets' do
+    erb :'tweets/tweets.erb'
   end
 
 end
